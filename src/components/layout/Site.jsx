ca3fa23b6ad2b794e6ae0f17969803b2b6015662
Jane Valencia <jv.janevalencia@gmail.com>
2023-05-17 11:37:19 +0800

    Remove gradient, and blur

--- src/components/layout/Site.jsx
+++ src/components/layout/Site.jsx
@@ -5,7 +5,7 @@ const SiteLayout = ({ children }) => {
     return (
         <>
             {/* Overlay Gradient */}
-            <div className='absolute w-[650px] h-[650px] rounded-full left-[-15%] top-[-30%] -z-10 bg-gradient-to-br from-gradient-blue to-white'></div>
+            <div className='absolute w-[650px] h-[650px] rounded-full left-[-15%] top-[-30%] -z-10 bg-gradient-blue blur-3xl'></div>
             <Header />
             <main>{children}</main>
             <Footer />
