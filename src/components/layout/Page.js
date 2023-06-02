aa3da645ec3dec13054d9e4ddda687ff3b290b45
Jane Valencia <jv.janevalencia@gmail.com>
2023-06-02 19:23:30 +0800

    Update PageLayout rendering different section type

--- src/components/layout/Page.js
+++ src/components/layout/Page.js
@@ -1,11 +1,32 @@
 import Hero from '@/components/Hero';
 import SEO from '@/components/Seo';
+import { Stack, StackSplit, Split, SplitReversed } from '@/components/sections';
 
-const PageLayout = ({ seo, ...page }) => {
+const PageLayout = ({ seo, sections, ...page }) => {
     return (
         <>
             {seo && <SEO {...seo} />}
+
             {page.hero && <Hero hero={page.hero} />}
+
+            {sections && (
+                <>
+                    {sections.map((section) => {
+                        switch (section.layout) {
+                            case 'Stack_Split':
+                                return <StackSplit key={section.sectionTitle} {...section} />;
+                            case 'Stack':
+                                return <Stack key={section.sectionTitle} {...section} />;
+                            case 'Split':
+                                return <Split key={section.sectionTitle} {...section} />;
+                            case 'Split_Reversed':
+                                return <SplitReversed key={section.sectionTitle} {...section} />;
+                            default:
+                                return null;
+                        }
+                    })}
+                </>
+            )}
         </>
     );
 };
