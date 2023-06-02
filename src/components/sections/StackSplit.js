c04d93d39d853ba837379398ae7f51da52218083
Jane Valencia <jv.janevalencia@gmail.com>
2023-06-02 21:46:11 +0800

    Add background theme

--- src/components/sections/StackSplit.js
+++ src/components/sections/StackSplit.js
@@ -13,42 +13,46 @@ const StackSplit = ({
     theme,
 }) => {
     return (
-        <section className='container py-24'>
-            <h3
-                className={'text-center mb-3 ' + sectionTheme.subheading[theme]}
-            >
-                {smallHeading}
-            </h3>
-            <h2 className='text-center'>{sectionTitle}</h2>
-            <div className='flex flex-col items-center mt-24 gap-20 lg:flex-row lg:justify-between lg:gap-11'>
-                <div className='relative flex flex-col items-center w-2/5'>
-                    <Image
-                        src='/images/bg-circle.png'
-                        alt='Overlay Circle'
-                        width={500}
-                        height={500}
-                    />
-                    <div className='absolute top-[-20%] z-10 w-3/4'>
+        <section className={sectionTheme.background[theme]}>
+            <section className='container py-24'>
+                <h3
+                    className={
+                        'text-center mb-3 ' + sectionTheme.subheading[theme]
+                    }
+                >
+                    {smallHeading}
+                </h3>
+                <h2 className='text-center'>{sectionTitle}</h2>
+                <div className='flex flex-col items-center mt-24 gap-20 lg:flex-row lg:justify-between lg:gap-11'>
+                    <div className='relative flex flex-col items-center w-2/5'>
                         <Image
-                            src={images[0].url}
-                            alt={`${sectionTitle}-image`}
+                            src='/images/bg-circle.png'
+                            alt='Overlay Circle'
                             width={500}
-                            height={525}
+                            height={500}
                         />
+                        <div className='absolute top-[-20%] z-10 w-3/4'>
+                            <Image
+                                src={images[0].url}
+                                alt={`${sectionTitle}-image`}
+                                width={500}
+                                height={525}
+                            />
+                        </div>
+                    </div>
+                    <div className='w-full lg:w-3/4'>
+                        <div
+                            className={
+                                'font-normal text-2xl mb-8 ' +
+                                sectionTheme.description[theme]
+                            }
+                        >
+                            <MDXRemote {...sectionDescription.mdx} />
+                        </div>
+                        <Button {...ctaButton} />
                     </div>
                 </div>
-                <div className='w-full lg:w-3/4'>
-                    <div
-                        className={
-                            'font-normal text-2xl mb-8 ' +
-                            sectionTheme.description[theme]
-                        }
-                    >
-                        <MDXRemote {...sectionDescription.mdx} />
-                    </div>
-                    <Button {...ctaButton} />
-                </div>
-            </div>
+            </section>
         </section>
     );
 };
--- src/libs/_theme.js
+++ src/libs/_theme.js
@@ -1,4 +1,10 @@
 export const sectionTheme = {
+    background: {
+        Transparent: 'bg-gradient-to-l from-gradient-blue from-10% to-white to-30%',
+        Blue: '',
+        Green: '',
+        White_Yellow: ''
+    },
     subheading: {
         Transparent: 'text-primary-blue',
         Blue: '',
