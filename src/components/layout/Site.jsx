72736bb8d292d9bea3de6b6ab447bf6f149d1a6c
Jane Valencia <jv.janevalencia@gmail.com>
2023-06-02 14:16:24 +0800

    Fix the responsive on background overlay in SiteLayout

--- src/components/layout/Site.jsx
+++ src/components/layout/Site.jsx
@@ -9,7 +9,7 @@ const SiteLayout = ({ children }) => {
                 <link rel='shortcut icon' href='/logo.svg' />
             </Head>
             {/* Overlay Gradient */}
-            <div className='absolute w-[650px] h-[650px] rounded-full left-[-15%] top-[-30%] -z-10 bg-gradient-blue blur-[150px]'></div>
+            <div className='absolute w-full sm:w-[650px] h-[650px] rounded-full left-[-15%] top-[-30%] -z-10 bg-gradient-blue blur-[150px]'></div>
             <Header />
             <main>{children}</main>
             <Footer />
